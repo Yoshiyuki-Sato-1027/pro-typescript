65589bbc25482e433c137b2051a757e2ca5ecbe2
satou-yosiyuki <stuysyk1027@icloud.com>
2024-07-03 22:31:14 +0900

    class

--- index.ts
+++ index.ts
@@ -4,3 +4,14 @@ function repeatHello(count: number): string {
 }
 
 repeatHello(1);
+
+// クラスで書く場合
+class Human {
+  greet() {
+    console.log("Hello");
+  }
+}
+
+const human = new Human();
+
+human.greet();
